75a8cea4e82ec3dbbccf60df69e74749550867f8
machinemaths <aadarshbh1112@gmail.com>
2025-05-06 00:14:28 -0400

    added

---
+++ sqlquery.sql
@@ -0,0 +1,39 @@
+USE nepal_hotels;
+SELECT * FROM cities;
+SELECT * FROM peak_seasons;
+SELECT * FROM users;
+SELECT * FROM hotels;
+SELECT * FROM room_types;
+SELECT * FROM room_capacities;
+SELECT * FROM bookings;
+SELECT * FROM booking_guests;
+SELECT * FROM payments;
+SELECT * FROM room_amenities;
+SELECT * FROM hotel_amenities;
+SELECT * FROM testimonials;
+SELECT * FROM special_offers;
+SELECT * FROM offer_tags;
+SELECT * FROM offer_features;
+SELECT * FROM booking_constraints;
+SELECT * FROM advance_booking_discounts;
+SELECT * FROM user_roles;
+SELECT * FROM room_availability;
+SELECT * FROM report_types;
+SELECT * FROM generated_reports; 
+SELECT * FROM hotel_config;
+SELECT * FROM audit_logs;
+SELECT * FROM room_inventory;
+SELECT * FROM room_prices;
+SELECT * FROM daily_room_availability;
+SELECT * FROM rooms;
+SELECT * FROM cancellation_charges;
+SELECT * FROM cancellation_policies;
+SELECT * FROM refunds;
+SELECT * FROM room_pricing;
+SELECT * FROM confirmation_views;
+SELECT * FROM booking_status_history;
+
+
+
+
+
